e6c1558f80ba5caceea6216c2d32284841e8e58d
John Bruce <jbruce2112@gmail.com>
2017-07-18 21:10:40 -0400

    Initial Commit

---
+++ HealthTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthTracker
+//
+//  Created by John on 7/18/17.
+//  Copyright © 2017 Bruce32. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
